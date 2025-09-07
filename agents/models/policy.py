643123442d1a8641d1d38a360a0ffbd11559ae84
aceniccola <andrewsamce@me.com>
2025-09-06 22:21:07 -0400

    policy

--- agents/models/policy.py
+++ agents/models/policy.py
@@ -7,6 +7,17 @@ import torchvision.transforms as transforms
 
 # this is the policy network that makes decisions about what to do in the world
 class PolicyModel(nn.Module):
-    def __init__(self):
+    def __init__(self, embedding_size):
         super(PolicyModel, self).__init__()
         self.fc1 = nn.Linear(64*64, 128)
+        self.activation1 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 10)
+        self.activation2 = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.activation1(x)
+        x = self.fc2(x)
+        x = self.activation2(x)
+        print(x)
+        return x
